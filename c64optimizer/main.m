3ec12fbab6f2738ba3b184735ed76b844cf68040
Jonatan <hvorblevden@gmail.com>
2023-03-07 09:15:52 +0100

    Initial Commit

---
+++ c64optimizer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  c64optimizer
+//
+//  Created by Jonatan Yde on 07/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
